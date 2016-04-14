65a053fcd829b3dc0ce80922880ff7b13ca40c6c
AnilSingh79 <singh.ap79@gmail.com>
2016-04-13 21:33:28 -0500

    Update pydan_test1.py

--- pydan/pytest/pydan_test1.py
+++ pydan/pytest/pydan_test1.py
@@ -13,13 +13,11 @@ Created on Nov 19, 2015
 from pydan.pycsv import pycsv_reader
 from pydan.pytools import month_enum, print_error,clean_string
 from pypaint.pygraph import TGraph
-   
+from matplotlib import pyplot as plt
+from matplotlib import ticker
+from datetime import datetime
+
 def preAnalysis():
-    '''
-     Most of these things should be done, interactively. 
-     They give a flavor of what data looks like.
-    '''
-    
 #   ##Initialize the csv reader
     csv = pycsv_reader("C:\\Users\\SingAn22\\Desktop\\hcsvreader\\533098.csv")
     ##Load the data into sqlite database, obtain pydset object.
@@ -27,15 +25,17 @@ def preAnalysis():
                            database ='C:\\Users\\SingAn22\\Desktop\\hcsvreader\\dset.db')
     ##Print the column names from data 
     data.columns()
+    data.head(5)
+    
     ##At the moment every element is a string, so analyze what datatypes can be associated with each string
     #Missing values are represented as empty strings, and they dont effect this operation.
-    colTypes = data.analyze_columns()   
-    for col in colTypes: print col,'   ', colTypes[col]                                  
+                                 
     ##Let us print first few lines close to top... 
    
     ##We saw that 'Dat' column is not in a format recognised by sqlite. So we need to fix it.     
     #####1. Come up with a format fixing function.
     #####2. Update the Dat column by applying fixing function to each element. 
+    '01-SEPTEMBER-2014'
     def refine_date(element):
         try:
             myDate = element.split('-')
@@ -46,54 +46,49 @@ def preAnalysis():
             return newDate
         except Exception as err:
             print_error(err,'pydan_test1.refine_date')
+    
     ##Now time to apply this function to the col=Dat.
-    data.apply(colName='Dat', funcName='to_date', func=refine_date, numparams=1) 
+    data.apply(colName='Dat', funcName='to_date', func=refine_date, 
+               argNames=['Dat'])
+    
     ##Now see how the Dat column looks like.
     data.head(5)
-    ##Analyze the columns-types again.
+  
+    ########Analyze the columns-types again.
+    #for col in colTypes: print col,'   ', colTypes[col]    
     colTypes = data.analyze_columns()
-    for col in colTypes: print col,'    ', colTypes[col]
-    
+    for col in colTypes: print col,'    ', colTypes[col]    
     dset = data.transform(resultTab='pydanTable', resultType=' TABLE ', 
-                                    colTypes=colTypes, returnQueryOnly=False)
+                             colTypes=colTypes, returnQueryOnly=False)
+    ##Drop the initial table.
     data.drop()
     data = None    
     return dset
-        
+      
 def Main():
     dset = preAnalysis()
+    
     dset.columns()
     dset.describe()
     #for c in dset.colTypes: print c, "   ",dset.colTypes[c]
     ##Now let is aggregate the information to year-month-week level.
     date = '2015-10-01'
-    from datetime import datetime
+    
     def month(date):
         return int(datetime.strptime(str(date),'%Y-%m-%d').date().strftime('%m'))
     def week(date):
         return int(datetime.strptime(str(date),'%Y-%m-%d').date().strftime('%W'))
     def year(date):
         return int(datetime.strptime(str(date),'%Y-%m-%d').date().strftime('%Y'))
-    dt2 = dset.subset(
-                      resultTab='weekly_temp',
-                      resultType=' TABLE ',
-                      varNames=[
-                                'Dat  Yearr',
-                                'Dat  Monthh',
-                                'Dat Weekk',
-                                'NoofShares',
-                                'ClosePrice',
-                                'TotalTurnoverRs',
-                                'LowPrice',
-                                'HighPrice',
-                                'SpreadCloseOpen'
-                                ]
-                      )
-    
-    dt2.apply(colName='Yearr',funcName='to_year',func=year,numparams=1)
-    dt2.apply(colName='Monthh',funcName='to_month',func=month,numparams=1)
-    dt2.apply(colName='Weekk',funcName='to_week',func=week,numparams=1)
-    dt3 = dt2.aggregate(resultTab='WEEKLY',resultType=' TABLE ',aggOp=' AVG ', 
+    def uniqueWeek(weekOfYear, year):
+        u= 100*int(year)+1*int(weekOfYear)
+        print u
+        return u
+    dset.addColumn("myYear","TEXT",'to_year',year,argNames=['Dat'])
+    dset.addColumn("myMonth","TEXT",'to_month',month,argNames=['Dat'])
+    dset.addColumn("myWeek","TEXT",'to_week',week,argNames=['Dat'])
+    dset.addColumn("unqWeek","NUMBER","to_unwk",uniqueWeek,argNames=['myWeek','myYear'])
+    dt3 = dset.aggregate(resultTab='WEEKLY',aggOp=' AVG ', 
                         varNames=[
                                 'NoofShares',
                                 'ClosePrice',
@@ -102,31 +97,44 @@ def Main():
                                 'HighPrice',
                                 'SpreadCloseOpen'
                                 ],
-                        groupBy=['Yearr','Monthh','Weekk'],
-                        orderBy = ['Yearr','Monthh','Weekk'],
-                        conditions = ['Yearr=2012']
+                        groupBy=['unqWeek'],
+                        orderBy = ['unqWeek'],
+                        conditions = ['1=1'],
+                                   
                         )
-    
+    dt3.to_csv("C:\\Users\\SingAn22\\Desktop\\hcsvreader\\out.csv")
+   
+
     gr = dt3.graph1D(
                     outfile='o.db',
-                    varNameX='Weekk',
-                    varNameY='NoofShares', 
-                    plotName='test',
-                    plotTitle='test'
+                    varNameX='unqWeek',
+                    varNameY='LowPrice', 
+                    plotName='lPrice_Graph',
+                    plotTitle='Low Price (Weekly)'
                       )
+    gr2 = dt3.graph1D(
+                    outfile='o.db',
+                    varNameX='unqWeek',
+                    varNameY='HighPrice', 
+                    plotName='hPrice_Graph',
+                    plotTitle='High Price (Weekly)'
+                    )
     
-    from matplotlib import pyplot as plt
+    
     fig = plt.figure()
     ax  = fig.add_subplot(1,1,1)
     ax.set_axis_bgcolor('white')
-    ##ax.format_xdata = DateFormatter('%Y')
-    gr.get_cosmetics().xMin = 1
-    gr.get_cosmetics().xMax = 60
+    
     gr.get_cosmetics().lineWidth=2
-    ##gr.get_costetis().yMax = 100000
-    gr.get_cosmetics().color='blue'
-   
+    gr.get_cosmetics().color='blue'   
+    gr2.get_cosmetics().color='red'
     gr.draw(ax)
+    gr2.draw(ax)
+    def make_label(value,pos):
+        return str(int(value/100))+"-"+str(int(value-int(value/100)*100))
+    
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(make_label))
+    ax.grid(True)
     plt.show()
                   
     #dt = datetime.strptime(date, "%Y-%m-%d")
@@ -136,7 +144,5 @@ def Main():
     #d = csv.to_database(tabName='idiot', database='C:\\Users\\SingAn22\\Desktop\\hcsvreader\\tester.db',varTypes={'var':'NUMBER'})
     #d.describe()
     #print 'percentile: ',d.quantile('var',0.5)
-    
-    
-    
+
 Main()
